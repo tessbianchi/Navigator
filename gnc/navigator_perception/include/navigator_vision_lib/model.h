63b8e877cbaa6c064446fb0d4219a971068e605f
Tess <tesshbianchi@gmail.com>
2016-08-30 02:15:25 -0400

    SCAN THE CODE: optimizes the algorithm so that it doesn't have to check for every combination of four points

--- gnc/navigator_perception/include/navigator_vision_lib/model.h
+++ gnc/navigator_perception/include/navigator_vision_lib/model.h
@@ -1,16 +1,26 @@
 #ifndef MODEL_H
 #define MODEL_H
-#include <string>
 #include <vector>
+#include <math.h>
+#include <map>
+#include <Eigen/Core>
 
 class Model
 {
 public:
-    Model(float width, float height);
+    Model(float width, float height, int min_points);
     ~Model();
-     float width, height;
+    float width, height;
+    int min_points;
+    bool check_point(Eigen::Vector3d point);
+    void remove_point(Eigen::Vector3d point);
+    bool get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<Eigen::Vector3d>& model2d);
+    bool complete();
 
 private:
-
+    std::vector<std::vector<Eigen::Vector3d>> potential_models;
+    std::vector<Eigen::Vector3d> current_points;
+    std::vector<float> unused_distances;
+    std::map<Eigen::Vector3d *, float> point_to_distance;
 };
 #endif // MODEL_H
